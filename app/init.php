b23ade5c99aba64895dbc8bfe49fee7a0580c3c2
Mitch Hijlkema <mitchhijlkema@hotmail.com>
2019-01-09 11:31:09 +0100

    added theme options
    Signed-off-by: Mitch Hijlkema <mitchhijlkema@hotmail.com>

--- app/init.php
+++ app/init.php
@@ -23,3 +23,22 @@ function vmw_sidebar_init() {
 	) );
 }
 add_action( 'widgets_init', 'vmw_sidebar_init' );
+
+
+if( function_exists('acf_add_options_page') ) {
+
+	acf_add_options_page([
+		'page_title' 	=> 'Theme Options',
+		'menu_title'	=> 'Theme Options',
+		'menu_slug' 	=> 'theme-options',
+		'capability'	=> 'edit_posts',
+		'redirect'		=> false
+	]);
+
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'Product settings',
+		'menu_title'	=> 'Product',
+		'parent_slug'	=> 'theme-options',
+	));
+	
+}
