d54a7e23ebdf553c055e7039064074b38eab2c92
Mitch Hijlkema <mitchhijlkema@hotmail.com>
2019-01-14 09:33:35 +0100

    made minus live
    Signed-off-by: Mitch Hijlkema <mitchhijlkema@hotmail.com>

--- js/plus-minus.js
+++ js/plus-minus.js
@@ -4,6 +4,22 @@ class PlusMinus {
         this.main()
     }
 
+
+    // helper for enabling IE 8 event bindings
+    addEvent(el, type, handler) {
+        if (el.attachEvent) el.attachEvent('on'+type, handler); else el.addEventListener(type, handler);
+    }
+
+    // live binding helper
+    live(selector, event, callback, context) {
+        addEvent(context || document, event, function(e) {
+            var found, el = e.target || e.srcElement;
+            while (el && !(found = el.id == selector)) el = el.parentElement;
+            if (found) callback.call(el, e);
+        });
+    }
+
+
     main() {
         document.querySelectorAll('.js-make-me-less').forEach( el => {
             this.addButtonListeners( el.parentElement )
@@ -13,12 +29,19 @@ class PlusMinus {
     addButtonListeners( form ) {
         let input = form.querySelector('#quantity')
 
-        form.querySelector('.js-make-me-less').addEventListener('click', (event) => {
+        this.live(form.querySelector('.js-make-me-less'), 'click', (event) => {
             if( parseInt(input.value) <= 1) {
                 return;
             }
             input.value--
         })
+
+        // form.querySelector('.js-make-me-less').addEventListener('click', (event) => {
+        //     if( parseInt(input.value) <= 1) {
+        //         return;
+        //     }
+        //     input.value--
+        // })
 
         form.querySelector('.js-make-me-more').addEventListener('click', (event) => {
             // if( parseInt(input.value) <= 1) {
