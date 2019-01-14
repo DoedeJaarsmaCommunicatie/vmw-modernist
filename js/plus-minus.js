725cda578e6ce8ba2744b321daf4b46b16d9b928
Mitch Hijlkema <mitchhijlkema@hotmail.com>
2019-01-14 10:07:08 +0100

    refresh clicks if not enabled
    Signed-off-by: Mitch Hijlkema <mitchhijlkema@hotmail.com>

--- js/plus-minus.js
+++ js/plus-minus.js
@@ -15,35 +15,36 @@ class PlusMinus {
     addButtonListeners( form ) {
         let input = form.querySelector('#quantity')
 
-        form.querySelector('.js-make-me-less').addEventListener('click', () => {
-            if( parseInt(input.value) <= 1) {
-                return;
-            }
-            input.value--
-        })
+        if( !input.getAttribute('data-event') ) {
+            form.querySelector('.js-make-me-less').addEventListener('click', () => {
+                if( parseInt(input.value) <= 1) {
+                    return;
+                }
+                input.value--
+            })
 
-        form.querySelector('.js-make-me-more').addEventListener('click', () => {
-            input.value++
-        })
+            form.querySelector('.js-make-me-more').addEventListener('click', () => {
+                input.value++
+            })
+
+            input.setAttribute('data-event', 'on')
+
+        }
+
     }
 
     watchForChanges() {
         let observer = new MutationObserver((mutationsList, observer) => {
             for( let mutation of mutationsList) {
                 if (mutation.type == 'childList') {
-                    console.log('A child node has been added or removed.');
+                    document.querySelectorAll('.js-make-me-less').forEach( el => {
+                        this.addButtonListeners( el.parentElement )
+                    })
                 }
-                else if (mutation.type == 'attributes') {
-                    console.log('The ' + mutation.attributeName + ' attribute was modified.');
-                } else if( mutation.type == 'subtree') {
-                    console.log( mutation )
-                }
-
-                console.log(mutation)
             }
         })
 
-        observer.observe(document.querySelector('.products'), { attributes: true, childList: true, subtree: true })
+        observer.observe(document, { attributes: true, childList: true, subtree: true })
     }
 }
 
