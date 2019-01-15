bfeda7f3d430c1ade5874a5e45c0bd436c1c9b4b
Mitch Hijlkema <mitchhijlkema@hotmail.com>
2019-01-15 15:04:26 +0100

    logged out fix
    Signed-off-by: Mitch Hijlkema <mitchhijlkema@hotmail.com>

--- js/Kiyoh.js
+++ js/Kiyoh.js
@@ -1,30 +1,33 @@
 function vmw_request() {
-    let xhr = new XMLHttpRequest();
+	let xhr = new XMLHttpRequest();
 
-    xhr.open('get', 'https://vindmijnwijn.nl/vmw_new_staging/wp-admin/admin-post.php?action=fetch_kiyoh')
+	xhr.open(
+		"get",
+		"https://vindmijnwijn.nl/wp-admin/admin-post.php?action=fetch_kiyoh"
+	);
 
-    xhr.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            window.vmw = {};
-            try {
-                window.vmw = JSON.parse(this.response).data;
-                console.log(window.vmw);
-                let el = document.querySelector('[data-score]');
-                el.dataset.score = window.vmw.total_score
-                el.innerHTML = el.dataset.score;
-            } catch (e) {
-                console.warn(e);
-            }
-        }
-    }
+	xhr.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			window.vmw = {};
+			try {
+				window.vmw = JSON.parse(this.response).data;
+				console.log(window.vmw);
+				let el = document.querySelector("[data-score]");
+				el.dataset.score = window.vmw.total_score;
+				el.innerHTML = el.dataset.score;
+			} catch (e) {
+				console.warn(e);
+			}
+		}
+	};
 
-    try {
-        xhr.send();
-    } catch ( e ) {
-        console.warn(e)
-    }
+	try {
+		xhr.send();
+	} catch (e) {
+		console.warn(e);
+	}
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    vmw_request();
-})
+document.addEventListener("DOMContentLoaded", () => {
+	vmw_request();
+});
