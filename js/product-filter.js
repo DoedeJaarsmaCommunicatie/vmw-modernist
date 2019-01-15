e53b13d4d20c3fe56a34ea4646d7edc159098d73
Mitch Hijlkema <mitchhijlkema@hotmail.com>
2019-01-15 15:34:55 +0100

    fix js
    Signed-off-by: Mitch Hijlkema <mitchhijlkema@hotmail.com>

--- js/product-filter.js
+++ js/product-filter.js
@@ -1,9 +1,5 @@
-(function($) {
-	document.addEventListener("DOMContentLoaded", function() {
-		$("#toggleFilter").on("click", function() {
-			document
-				.querySelector("#filterProducts")
-				.classList.toggle("active");
-		});
+document.addEventListener("DOMContentLoaded", function() {
+	document.querySelector("#toggleFilter").on("click", function() {
+		document.querySelector("#filterProducts").classList.toggle("active");
 	});
-})(jQuery);
+});
