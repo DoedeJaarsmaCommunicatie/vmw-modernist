8890e2f7d2504ef49f6ff07784a1bec42fc17e0d
Mitch Hijlkema <mitchhijlkema@hotmail.com>
2019-01-18 10:58:39 +0100

    price typo
    Signed-off-by: Mitch Hijlkema <mitchhijlkema@hotmail.com>

--- header.php
+++ header.php
@@ -11,7 +11,7 @@ ob_start();
 
 if( isset($_GET['product-price'])) {
 	global $wp_query;
-	$pice = explode( '-', $_GET['product-price']);
+	$price = explode( '-', $_GET['product-price']);
 	$wp_query->set('meta_query', [
 		[
 			'key'     => '_regular_price',
